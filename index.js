82d7fb2cb4b9cc719051c05cfd44dee6be349c07
이재석 <jslee01@miridih.com>
2023-07-26 09:09:17 +0900

    feat: 시작시 룰렛 생성

--- index.js
+++ index.js
@@ -1,28 +1,62 @@
-document.getElementById("create-wheel").addEventListener("click", function () {
-  var numSegments = document.getElementById("num-segments").value;
-  var segmentNames = document
-    .getElementById("segment-names")
-    .value.split(",")
-    .map((name) => name.trim());
-  if (segmentNames.length !== Number(numSegments)) {
-    alert("Please enter exactly " + numSegments + " segment names.");
-    return;
-  }
+const segmentNames = [
+  "지하",
+  "KFC",
+  "고씨네",
+  "누리한방삼계탕",
+  "담미온",
+  "맥도날드",
+  "명품들깨칼국수",
+  "바디쉐프",
+  "버거리",
+  "북촌손만두",
+  "생어거스틴",
+  "소용궁",
+  "슬로우캘리",
+  "오한수 우육면가",
+  "윤가네칼국수",
+  "장터",
+  "제라진",
+  "진순대국",
+  "천하제육",
+  "청해동태탕",
+  "취쓰부",
+  "큰맘할매순대국",
+  "투마마김치찌개",
+  "한옥집김치찜",
+  "한촌설렁탕",
+];
 
-  var colors = [
-    "#ff0000",
-    "#ff7f00",
-    "#ffff00",
-    "#00ff00",
-    "#0000ff",
-    "#4B0082",
-    "#9400D3",
-    "#8B4513",
-    "#A52A2A",
-    "#D2691E",
-    "#8B008B",
-    "#00008B",
-  ];
+const colors = [
+  "#ff0000",
+  "#ff7f00",
+  "#ffff00",
+  "#00ff00",
+  "#0000ff",
+  "#4B0082",
+  "#9400D3",
+  "#8B4513",
+  "#A52A2A",
+  "#D2691E",
+  "#8B008B",
+  "#00008B",
+  "#ff0000",
+  "#ff7f00",
+  "#ffff00",
+  "#00ff00",
+  "#0000ff",
+  "#4B0082",
+  "#9400D3",
+  "#8B4513",
+  "#A52A2A",
+  "#D2691E",
+  "#8B008B",
+  "#00008B",
+  "#ff0000",
+];
+
+const numSegments = segmentNames.length;
+
+const createWheel = () => {
   var data = Array.from({ length: numSegments }, (_, i) => ({
     color: colors[i % colors.length],
     name: segmentNames[i],
@@ -49,18 +83,13 @@ document.getElementById("create-wheel").addEventListener("click", function () {
     })
     .attr("dy", ".50em")
     .style("text-anchor", "middle")
+    .style("font-size", "1.5px")
     .text(function (d) {
       return d.data.name;
     });
-});
+};
 
 document.getElementById("spin-button").addEventListener("click", function () {
-  var numSegments = document.getElementById("num-segments").value;
-  var segmentNames = document
-    .getElementById("segment-names")
-    .value.split(",")
-    .map((name) => name.trim());
-
   var wheel = document.getElementById("wheel");
   var result = document.getElementById("result");
   var angle = Math.floor(Math.random() * 1080 + 720);
@@ -69,10 +98,14 @@ document.getElementById("spin-button").addEventListener("click", function () {
 
   // Wait for the transition to finish before calculating the result
   setTimeout(function () {
-    // Normalize the angle to be between 0 and 359
-    var normalizedAngle = angle % 360;
+    // Normalize the angle to be between 0 and 359, then align it to the top
+    var normalizedAngle = (angle - 90) % 360;
+    if (normalizedAngle < 0) normalizedAngle += 360; // if angle is negative, add 360 to normalize it
+
     // Calculate the result
     var resultSegment = Math.floor(normalizedAngle / (360 / numSegments));
     result.innerText = "You landed on " + segmentNames[resultSegment] + "!";
   }, 3000);
 });
+
+createWheel();
